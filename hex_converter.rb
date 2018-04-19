de9ffd520e553f89ef1655c00683c09363d40d6b
stemshell <amartin262@gmail.com>
2018-04-19 16:44:59 +1000

    Code passed tests after minor changes. Also added comments

--- hex_converter.rb
+++ hex_converter.rb
@@ -1,11 +1,14 @@
+# This class contains methods for converting strings of hexadecial digits into strings of their corresponding ASCII characters.
 class Hex
-  def generate_conversion_hash
+  # This method generates a hash which can be used to convert decimal integer values to ASCII characters
+  def self.generate_conversion_hash
     @conversion_hash = {}
     (('A'..'Z').zip(1..26)).concat(('a'..'z').zip(97..122)).each {|n| @conversion_hash[n[0]] = n[1]}
     @conversion_hash[' '] = 32
   end
 
-  def convert input
+  # This method takes the input string, splits and converts it into an array of decimal integers, and then returns an ASCII character for each (combined into a string)
+  def self.convert input
     generate_conversion_hash
     input.split.map(&:hex).map {|x| @conversion_hash.key(x)}.join
   end
--- spec/hex_converter_spec.rb
+++ spec/hex_converter_spec.rb
@@ -1,10 +1,22 @@
-require_relative 'hex_converter.rb'
+require_relative '../hex_converter.rb'
 
 RSpec.describe Hex do
   context "When testing the Hex class" do
     it "should return empty string from empty string input" do
       input = ''
       output = ''
+      expect(Hex.convert input).to eq output
+    end
+
+    it "should return 'A' from '01'" do
+      input = '01'
+      output = 'A'
+      expect(Hex.convert input).to eq output
+    end
+
+    it "should return 'It is Wednesday my dudet' from '09 74 20 69 73 20 17 65 64 6E 65 73 64 61 79 20 6D 79 20 64 75 64 65 74'" do
+      input = '09 74 20 69 73 20 17 65 64 6E 65 73 64 61 79 20 6D 79 20 64 75 64 65 74'
+      output = 'It is Wednesday my dudet'
       expect(Hex.convert input).to eq output
     end
   end
