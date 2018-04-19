5ed1122e7c0b7f51ac8de0ba4bdd4db7044b335d
stemshell <amartin262@gmail.com>
2018-04-19 22:27:47 +1000

    implemetnted method to encode in addition  to encoding, including additional test

--- hex_converter.rb
+++ hex_converter.rb
@@ -8,8 +8,14 @@ class Hex
   end
 
   # This method takes the input string, splits and converts it into an array of decimal integers, and then returns an ASCII character for each (combined into a string)
-  def self.convert input
+  def self.decode input
     generate_conversion_hash
     input.split.map(&:hex).map {|x| @conversion_hash.key(x)}.join
+  end
+
+  # This method takes the input string and returns a string of the hexadecimal ASCII codes for each character, with a space in between each one.
+  def self.encode input
+    generate_conversion_hash
+    input.chars.map {|x| @conversion_hash[x].to_s(16).rjust(2, '0')}.join(' ')
   end
 end
--- spec/hex_converter_spec.rb
+++ spec/hex_converter_spec.rb
@@ -1,23 +1,43 @@
 require_relative '../hex_converter.rb'
 
 RSpec.describe Hex do
-  context "When testing the Hex class" do
+  context "When testing the decode method" do
     it "should return empty string from empty string input" do
       input = ''
       output = ''
-      expect(Hex.convert input).to eq output
+      expect(Hex.decode input).to eq output
     end
 
     it "should return 'A' from '01'" do
       input = '01'
       output = 'A'
-      expect(Hex.convert input).to eq output
+      expect(Hex.decode input).to eq output
     end
 
     it "should return 'It is Wednesday my dudet' from '09 74 20 69 73 20 17 65 64 6E 65 73 64 61 79 20 6D 79 20 64 75 64 65 74'" do
       input = '09 74 20 69 73 20 17 65 64 6E 65 73 64 61 79 20 6D 79 20 64 75 64 65 74'
       output = 'It is Wednesday my dudet'
-      expect(Hex.convert input).to eq output
+      expect(Hex.decode input).to eq output
+    end
+  end
+
+  context "When testing the encode method" do
+    it "should return empty string from empty string input" do
+      input = ''
+      output = ''
+      expect(Hex.encode input).to eq output
+    end
+
+    it "should return '20' from ' '" do
+      input = ' '
+      output = '20'
+      expect(Hex.encode input).to eq output
+    end
+
+    it "should return '08 69' from 'Hi'" do
+      input = 'Hi'
+      output = '08 69'
+      expect(Hex.encode input).to eq output
     end
   end
 end
