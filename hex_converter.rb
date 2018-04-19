12d42ec2cb3623550f9ddb7ed4a3b461506af33e
stemshell <amartin262@gmail.com>
2018-04-19 11:29:46 +1000

    init commit

---
+++ hex_converter.rb
@@ -0,0 +1,13 @@
+class String
+  def numeric?
+    self =~ /\d/
+  end
+
+  def convert_hex
+    conversion_hash = {a: 10, b: 11, c: 12, d: 13, e: 14, f: 15}
+  end
+end
+
+def translate_input
+  input.split.map
+end
